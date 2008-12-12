870782d792254de6d8c3b73e4ba235a3e7085253
Ricardo Duarte <ricardo@ricardoduarte.com>
2008-12-11 22:56:37 -0200

    some validations on models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :unit_id
+  validates_presence_of :category_id
+  validates_numericality_of :unit_id, :only_integer => true
+  validates_numericality_of :category_id , :only_integer => true
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,4 @@
 class Unit < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :abbr
 end
