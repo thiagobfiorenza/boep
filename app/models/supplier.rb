80be6c424522670a959ea765910de8a68cdc0962
Niler Barcelos <Niler Barcelos>
2008-12-16 20:01:02 -0200

    Formulários comuns para edit e new

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,7 @@
 class Supplier < ActiveRecord::Base
+	validates_presence_of :name
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+	validates_length_of :phone, :in => 7..32, :allow_blank => false
+	validates_numericality_of :phone
+	validates_format_of :postcode, :with => /\d{8}/
 end
